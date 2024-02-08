cb2b2998528cd39a5572258d5d63dcd35bf20254
prova <prova@DESKTOP-EFS3A7T>
2024-02-08 23:33:34 +0100

    First commit

---
+++ src/main/java/com/mycompany/tcpex5bi/TCPex5BI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tcpex5bi;
+
+/**
+ *
+ * @author prova
+ */
+public class TCPex5BI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
